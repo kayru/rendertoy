66a8360bd3d833cc2408b559248816a5c0b33164
Tomasz Stachowiak <h3@h3.gd>
2017-02-06 03:05:25 +0100

    updated the build script

--- makeRelease.py
+++ makeRelease.py
@@ -8,6 +8,7 @@ os.mkdir('release')
 os.system('tools\\tundra2.exe release')
 shutil.copy('t2-output/win64-msvc-release-default/rendertoy.exe', 'release')
 shutil.copytree('data', 'release/data')
+shutil.copytree('tools/sublimePlugin', 'release/sublimePlugin')
 
 releaseVersion = '0.1'
 
